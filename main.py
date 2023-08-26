0780c7d75fd7506955d66812115f9bbd8dfd5d60
misygauziya <misygauziya@gmail.com>
2023-08-27 05:09:55 +0900

    [Update] guiの起動関数を分割/model,configをimport

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from vrct_gui import vrct_gui
+from config import config
+from model import model
 
 class VRCT():
     def __init__(self):
         pass
 if __name__ == "__main__":
-    # vrct_gui = VRCT_GUI()
-    vrct_gui.start()
+    vrct_gui.createUI()
+    vrct_gui.startMainLoop()
