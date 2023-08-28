84d76ed9a40c62aa5e8650d154c51718f30a0cc0
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2023-08-28 14:43:34 +0900

    Setting box: add items UI Scaling, Font Family, UI Language. add the function get_key_by_value to utils.py. and fixed a bit

--- utils.py
+++ utils.py
@@ -4,3 +4,9 @@ from PIL.Image import open as Image_open
 def getImageFile(file_name):
     img = Image_open(os_path.join(os_path.dirname(__file__), "img", file_name))
     return img
+
+def get_key_by_value(dictionary, value):
+    for key, val in dictionary.items():
+        if val == value:
+            return key
+    return None
