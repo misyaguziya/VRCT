21addd46ca49c7daff6fe78059196da14d6df197
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2023-10-08 14:15:20 +0900

    [Refactor] UiScalingManager: 変数名変更、汎用化。Entry widget以外でも使いたくなったので。

--- vrct_gui/config_window/widgets/createSideMenuAndSettingsBoxContainers/setting_box_containers/setting_box_translation/createSettingBox_Translation.py
+++ vrct_gui/config_window/widgets/createSideMenuAndSettingsBoxContainers/setting_box_containers/setting_box_translation/createSettingBox_Translation.py
@@ -16,7 +16,7 @@ def createSettingBox_Translation(setting_box_wrapper, config_window, settings, v
         for_var_label_text=view_variable.VAR_LABEL_DEEPL_AUTH_KEY,
         for_var_desc_text=view_variable.VAR_DESC_DEEPL_AUTH_KEY,
         entry_attr_name="sb__entry_deepl_authkey",
-        entry_width=settings.uism.SB__ENTRY_WIDTH_300,
+        entry_width=settings.uism.RESPONSIVE_UI_SIZE_INT_300,
         entry_bind__Any_KeyRelease=lambda value: deepl_authkey_callback(value),
         entry_textvariable=view_variable.VAR_DEEPL_AUTH_KEY,
     )
