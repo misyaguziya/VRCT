379e9164281b0c5f4a4eddade77ed28c077bb7fd
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2023-10-11 13:34:58 +0900

    [Update] Add Splash Screen: 起動中に何かしらの表示をして、ユーザーへのフィードバック。画像は仮置き。

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from vrct_gui.splash_window import SplashWindow
+splash = SplashWindow()
+splash.showSplash()
+
 import controller
 
 if __name__ == "__main__":
     controller.createMainWindow()
+    splash.destroySplash()
     controller.showMainWindow()
