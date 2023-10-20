ff0e13caf03f86ce07cce0420989deab6ca6c206
misyaguziya <misyaguziya@gmail.com>
2023-10-20 10:36:03 +0900

    👍[Update] Main : error.logを出力するように機能を追加

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import ctypes
-ctypes.windll.shcore.SetProcessDpiAwareness(0)
-
-from vrct_gui.splash_window import SplashWindow
-splash = SplashWindow()
-splash.showSplash()
-
-import controller
-
 if __name__ == "__main__":
-    controller.createMainWindow()
-    splash.destroySplash()
-    controller.showMainWindow()
+    try:
+        import ctypes
+        ctypes.windll.shcore.SetProcessDpiAwareness(0)
+
+        from vrct_gui.splash_window import SplashWindow
+        splash = SplashWindow()
+        splash.showSplash()
+
+        import controller
+        controller.createMainWindow()
+        splash.destroySplash()
+        controller.showMainWindow()
+
+    except Exception as e:
+        import traceback
+        with open('error.log', 'a') as f:
+            traceback.print_exc(file=f)
