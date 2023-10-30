1cf59501b3be0df7d976542f4a40b0978323e8ea
misyaguziya <misyaguziya@gmail.com>
2023-10-30 20:29:18 +0900

    👍[Update] installer : autoupdateの処理を変更

--- batch/update.bat
+++ batch/update.bat
@@ -1,11 +1,16 @@
 @if not "%~0"=="%~dp0.\%~nx0" start /min cmd /c,"%~dp0.\%~nx0" %* & goto :eof
 
-taskkill /im %1 /F
+set exe_name=%1
+set folder_name=%2
+set tmp_name=%3
+set restart=%4
+
+taskkill /im %exe_name% /F
 ping -n 2 127.0.0.1 > nul
-del /f %1
-ping -n 2 127.0.0.1 > nul
-rename %2 %1
-ping -n 2 127.0.0.1 > nul
-if %3 == True (
-    START "" %1
+del /f %exe_name%
+rmdir /s %folder_name%
+move %tmp_name%\* .\
+del /f %tmp_name%
+if %restart% == True (
+    START "" %exe_name%
 )
