74bd06c768f1bdb67d02d4ff05e2fcccf2c3724e
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2023-12-20 01:07:42 +0900

    [Update] Add feature, Send Only Translated Messages: 翻訳オン時、翻訳後のメッセージのみを送信する機能を追加。

--- vrct_gui/config_window/widgets/createSideMenuAndSettingsBoxContainers/setting_box_containers/setting_box_others/createSettingBox_Others.py
+++ vrct_gui/config_window/widgets/createSideMenuAndSettingsBoxContainers/setting_box_containers/setting_box_others/createSettingBox_Others.py
@@ -14,6 +14,9 @@ def createSettingBox_Others(setting_box_wrapper, config_window, settings, view_v
     def checkbox_auto_clear_message_box_callback(checkbox_box_widget):
         callFunctionIfCallable(view_variable.CALLBACK_SET_ENABLE_AUTO_CLEAR_MESSAGE_BOX, checkbox_box_widget.get())
 
+    def checkbox_send_only_translated_messages_callback(checkbox_box_widget):
+        callFunctionIfCallable(view_variable.CALLBACK_SET_ENABLE_SEND_ONLY_TRANSLATED_MESSAGES, checkbox_box_widget.get())
+
     def checkbox_notice_xsoverlay_callback(checkbox_box_widget):
         callFunctionIfCallable(view_variable.CALLBACK_SET_ENABLE_NOTICE_XSOVERLAY, checkbox_box_widget.get())
 
@@ -39,6 +42,16 @@ def createSettingBox_Others(setting_box_wrapper, config_window, settings, view_v
         variable=view_variable.VAR_ENABLE_AUTO_CLEAR_MESSAGE_BOX,
     )
     config_window.sb__auto_clear_message_box.grid(row=row)
+    row+=1
+
+    config_window.sb__send_only_translated_messages = createSettingBoxCheckbox(
+        for_var_label_text=view_variable.VAR_LABEL_ENABLE_SEND_ONLY_TRANSLATED_MESSAGES,
+        for_var_desc_text=view_variable.VAR_DESC_ENABLE_SEND_ONLY_TRANSLATED_MESSAGES,
+        checkbox_attr_name="sb__checkbox_send_only_translated_messages",
+        command=lambda: checkbox_send_only_translated_messages_callback(config_window.sb__checkbox_send_only_translated_messages),
+        variable=view_variable.VAR_ENABLE_SEND_ONLY_TRANSLATED_MESSAGES,
+    )
+    config_window.sb__send_only_translated_messages.grid(row=row)
     row+=1
 
 
