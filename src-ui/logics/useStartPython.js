080939a853a1a6d94c404a302a78e222a20e5d40
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-07-29 22:16:12 +0900

    [Refactor] Separate 'useReceiveRoutes' function that is control the received path from Python and send to the specific function, React.

--- src-ui/logics/useStartPython.js
+++ src-ui/logics/useStartPython.js
@@ -1,34 +1,8 @@
 import { Command } from "@tauri-apps/api/shell";
 import { store } from "@store";
-
-import { useMainFunction } from "./useMainFunction";
-
+import { useReceiveRoutes } from "./useReceiveRoutes";
 export const useStartPython = () => {
-    const {
-        updateTranslationStatus,
-        updateTranscriptionSendStatus,
-        updateTranscriptionReceiveStatus,
-    } = useMainFunction();
-
-    const routes = {
-        "/controller/callback_toggle_translation": updateTranslationStatus,
-        "/controller/callback_toggle_transcription_send": updateTranscriptionSendStatus,
-        "/controller/callback_toggle_transcription_receive": updateTranscriptionReceiveStatus,
-    };
-
-    const receiveRoutes = (parsed_data) => {
-        if (parsed_data.status === "ok") {
-            const route = routes[parsed_data.id];
-            if (route) {
-                route({ data: parsed_data.data });
-            } else {
-                console.error(`Invalid path: ${parsed_data.id}`);
-            }
-        } else {
-            console.log("Received data status is not 'ok'.", parsed_data);
-        }
-    };
-
+    const { receiveRoutes } = useReceiveRoutes();
 
     const asyncStartPython = async () => {
         const command = Command.sidecar("bin/test");
