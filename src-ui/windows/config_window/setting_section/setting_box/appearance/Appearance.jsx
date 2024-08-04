6b93a7067e8664a1fa8ae96149dbd99735d03d4b
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-08-04 11:39:49 +0900

    [bugfix_2] to fix git ignorecase.

---
+++ src-ui/windows/config_window/setting_section/setting_box/appearance/Appearance.jsx
@@ -0,0 +1,46 @@
+import { useSettingBox } from "../components/useSettingBox";
+import { useSelectedMicDeviceStatus, useMicDeviceListStatus } from "@store";
+export const Appearance = () => {
+    const { currentSelectedMicDeviceStatus, updateSelectedMicDeviceStatus } = useSelectedMicDeviceStatus();
+    const { currentMicDeviceListStatus } = useMicDeviceListStatus();
+    const {
+        DropdownMenuContainer,
+        SliderContainer,
+        CheckboxContainer,
+        SwitchboxContainer,
+        EntryContainer,
+        ThresholdContainer,
+        RadioButtonContainer,
+        DeeplAuthKeyContainer,
+    } = useSettingBox();
+
+    const selectFunction = (selected_data) => {
+        const asyncFunction = () => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(selected_data.selected_id);
+                }, 3000);
+            });
+        };
+        updateSelectedMicDeviceStatus(asyncFunction);
+    };
+
+    return (
+        <>
+            <DropdownMenuContainer dropdown_id="mic_host" label="Mic Host/Driver" desc="description" selected_id="b" list={{a: "A", b: "B", c: "C"}} />
+            <DropdownMenuContainer dropdown_id="mic_device" label="Mic Device" desc="description" selected_id={currentSelectedMicDeviceStatus.data} list={currentMicDeviceListStatus} selectFunction={selectFunction} state={currentSelectedMicDeviceStatus.state} />
+
+            <SliderContainer label="Transparent" desc="description" min="0" max="3000"/>
+            <CheckboxContainer label="Transparent" desc="description" checkbox_id="checkbox_id_1"/>
+            <SwitchboxContainer label="Transparent" desc="description" switchbox_id="switchbox_id_1"/>
+
+            <RadioButtonContainer label="Transparent" desc="description" switchbox_id="radiobutton_id_1"/>
+
+            <EntryContainer width="20rem" label="Transparent" desc="description" switchbox_id="entry_id_1"/>
+
+            <ThresholdContainer label="Transparent" desc="description" id="mic_threshold"  min="0" max="3000"/>
+
+            <DeeplAuthKeyContainer label="Transparent" desc="description"/>
+        </>
+    );
+};
