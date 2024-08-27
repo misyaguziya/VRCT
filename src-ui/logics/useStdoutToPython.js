4ce4f35dfdfc5f09c3c4224223b72d589398b975
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-08-27 11:06:10 +0900

    [Refactor] Logic: Move object structure creation to the parent file.

--- src-ui/logics/useStdoutToPython.js
+++ src-ui/logics/useStdoutToPython.js
@@ -1,11 +1,14 @@
 import { store } from "@store";
 
 export const useStdoutToPython = () => {
-    const asyncStdoutToPython = async (value) => {
+    const asyncStdoutToPython = async (path, value) => {
+        let send_object = { endpoint: path };
+        if (value) send_object.data = value;
+
         // send to python
         const backend_subprocess = store.backend_subprocess;
         if (backend_subprocess) {
-            await backend_subprocess.write(JSON.stringify(value) + "\n").then(() => {
+            await backend_subprocess.write(JSON.stringify(send_object) + "\n").then(() => {
             }).catch((err) => {
                 console.log(err);
             });
