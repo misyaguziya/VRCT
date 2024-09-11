f6810117eb3cb142e16105a82482e8bb84b04621
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-09-11 23:50:36 +0900

    [Update] Config Page: Device Tab. Adjust designs.

--- src-ui/app/config_page/setting_section/setting_box/device/Device.jsx
+++ src-ui/app/config_page/setting_section/setting_box/device/Device.jsx
@@ -1,18 +1,13 @@
 import { useTranslation } from "react-i18next";
 import styles from "./Device.module.scss";
 import {
-    DropdownMenuContainer,
     ThresholdContainer,
 } from "../components/useSettingBox";
 export const Device = () => {
-
     return (
         <>
-            <DropdownMenuContainer_MicHost />
-            {/* <DropdownMenuContainer_MicDevice /> */}
-            <ThresholdContainer_Mic />
-            <DropdownMenuContainer_SpeakerDevice />
-            <ThresholdContainer_Speaker />
+            <Mic_Container />
+            <Speaker_Container />
         </>
     );
 };
@@ -26,7 +21,7 @@ import { useSelectedMicDevice } from "@logics_configs/useSelectedMicDevice";
 import { LabelComponent } from "../components/label_component/LabelComponent";
 import { DropdownMenu } from "../components/dropdown_menu/DropdownMenu";
 
-const DropdownMenuContainer_MicHost = () => {
+const Mic_Container = () => {
     const { t } = useTranslation();
     const { currentSelectedMicHost, setSelectedMicHost } = useSelectedMicHost();
     const { currentMicHostList, getMicHostList } = useMicHostList();
@@ -44,68 +39,52 @@ const DropdownMenuContainer_MicHost = () => {
 
 
     return (
-        <div className={styles.device_container}>
-            <LabelComponent label={t("config_page.mic_host.label")} />
+        <div className={styles.mic_container}>
+            <div className={styles.device_container}>
+                <LabelComponent label={t("config_page.mic_host_device.label")} />
+                <div className={styles.device_contents}>
+                    <div className={styles.device_dropdown_wrapper}>
+                        <p className={styles.device_dropdown_label}>{t("config_page.mic_host_device.label_host")}</p>
+                        <DropdownMenu
+                            dropdown_id="mic_host"
+                            selected_id={currentSelectedMicHost.data}
+                            list={currentMicHostList.data}
+                            selectFunction={selectFunction_host}
+                            openListFunction={getMicHostList}
+                            state={currentSelectedMicHost.state}
+                            style={{ maxWidth: "20rem", minWidth: "10rem" }}
+                        />
+                    </div>
 
-            <div className={styles.device_contents}>
-                <div className={styles.device_dropdown_wrapper}>
-                    <p className={styles.device_dropdown_label}>Host/Driver</p>
-                    <DropdownMenu
-                        dropdown_id="mic_host"
-                        selected_id={currentSelectedMicHost.data}
-                        list={currentMicHostList.data}
-                        selectFunction={selectFunction_host}
-                        openListFunction={getMicHostList}
-                        state={currentSelectedMicHost.state}
-                        style={{ maxWidth: "20rem", minWidth: "10rem" }}
-                    />
+                    <div className={styles.device_dropdown_wrapper}>
+                        <p className={styles.device_dropdown_label}>{t("config_page.mic_host_device.label_device")}</p>
+                        <DropdownMenu
+                            dropdown_id="mic_device"
+                            selected_id={currentSelectedMicDevice.data}
+                            list={currentMicDeviceList.data}
+                            selectFunction={selectFunction_device}
+                            openListFunction={getMicDeviceList}
+                            state={currentSelectedMicDevice.state}
+                        />
+                    </div>
                 </div>
-
-                <div className={styles.device_dropdown_wrapper}>
-                    <p className={styles.device_dropdown_label}>Device</p>
-                    <DropdownMenu
-                        dropdown_id="mic_device"
-                        label={t("config_page.mic_device.label")}
-                        selected_id={currentSelectedMicDevice.data}
-                        list={currentMicDeviceList.data}
-                        selectFunction={selectFunction_device}
-                        openListFunction={getMicDeviceList}
-                        state={currentSelectedMicDevice.state}
-                    />
-                </div>
+            </div>
+            <div className={styles.threshold_container}>
+                <ThresholdContainer
+                    label={t("config_page.mic_dynamic_energy_threshold.label_for_manual")}
+                    desc={t("config_page.mic_dynamic_energy_threshold.desc_for_manual")}
+                    id="mic_threshold"
+                    min="0"
+                    max="2000"
+                />
             </div>
         </div>
     );
 };
 
-// import { useMicDeviceList } from "@logics_configs/useMicDeviceList";
-// import { useSelectedMicDevice } from "@logics_configs/useSelectedMicDevice";
-// const DropdownMenuContainer_MicDevice = () => {
-//     const { t } = useTranslation();
-//     const { currentSelectedMicDevice, setSelectedMicDevice } = useSelectedMicDevice();
-//     const { currentMicDeviceList, getMicDeviceList } = useMicDeviceList();
-
-//     const selectFunction = (selected_data) => {
-//         setSelectedMicDevice(selected_data.selected_id);
-//     };
-
-
-//     return (
-//         <DropdownMenuContainer
-//             dropdown_id="mic_device"
-//             label={t("config_page.mic_device.label")}
-//             selected_id={currentSelectedMicDevice.data}
-//             list={currentMicDeviceList.data}
-//             selectFunction={selectFunction}
-//             openListFunction={getMicDeviceList}
-//             state={currentSelectedMicDevice.state}
-//         />
-//     );
-// };
-
 import { useSpeakerDeviceList } from "@logics_configs/useSpeakerDeviceList";
 import { useSelectedSpeakerDevice } from "@logics_configs/useSelectedSpeakerDevice";
-const DropdownMenuContainer_SpeakerDevice = () => {
+const Speaker_Container = () => {
     const { t } = useTranslation();
     const { currentSelectedSpeakerDevice, setSelectedSpeakerDevice } = useSelectedSpeakerDevice();
     const { currentSpeakerDeviceList, getSpeakerDeviceList } = useSpeakerDeviceList();
@@ -116,42 +95,32 @@ const DropdownMenuContainer_SpeakerDevice = () => {
 
 
     return (
-        <DropdownMenuContainer
-            dropdown_id="speaker_device"
-            label={t("config_page.speaker_device.label")}
-            selected_id={currentSelectedSpeakerDevice.data}
-            list={currentSpeakerDeviceList.data}
-            selectFunction={selectFunction}
-            openListFunction={getSpeakerDeviceList}
-            state={currentSelectedSpeakerDevice.state}
-        />
-    );
-};
-
-const ThresholdContainer_Mic = () => {
-    const { t } = useTranslation();
-
-    return (
-        <ThresholdContainer
-            label={t("config_page.mic_dynamic_energy_threshold.label_for_manual")}
-            desc={t("config_page.mic_dynamic_energy_threshold.desc_for_manual")}
-            id="mic_threshold"
-            min="0"
-            max="2000"
-        />
-    );
-};
-
-const ThresholdContainer_Speaker = () => {
-    const { t } = useTranslation();
-
-    return (
-        <ThresholdContainer
-            label={t("config_page.speaker_dynamic_energy_threshold.label_for_manual")}
-            desc={t("config_page.speaker_dynamic_energy_threshold.desc_for_manual")}
-            id="speaker_threshold"
-            min="0"
-            max="4000"
-        />
+        <div className={styles.speaker_container}>
+            <div className={styles.device_container}>
+                <LabelComponent label={t("config_page.speaker_device.label")} />
+                <div className={styles.device_contents}>
+                    <div className={styles.device_dropdown_wrapper}>
+                        <DropdownMenu
+                            dropdown_id="speaker_device"
+                            label={t("config_page.speaker_device.label")}
+                            selected_id={currentSelectedSpeakerDevice.data}
+                            list={currentSpeakerDeviceList.data}
+                            selectFunction={selectFunction}
+                            openListFunction={getSpeakerDeviceList}
+                            state={currentSelectedSpeakerDevice.state}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.threshold_container}>
+                <ThresholdContainer
+                    label={t("config_page.speaker_dynamic_energy_threshold.label_for_manual")}
+                    desc={t("config_page.speaker_dynamic_energy_threshold.desc_for_manual")}
+                    id="speaker_threshold"
+                    min="0"
+                    max="4000"
+                />
+            </div>
+        </div>
     );
 };
