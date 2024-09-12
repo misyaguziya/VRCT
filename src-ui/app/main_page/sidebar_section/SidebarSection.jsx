6b3ff1f3b2ff0eb1f3c04add6fc5cd72ddf6a551
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-09-12 16:50:47 +0900

    [Update] Main Page: Side bar scroll bar. Adjust scroll bar color when language selector is opened.(For hiding in appearance)

--- src-ui/app/main_page/sidebar_section/SidebarSection.jsx
+++ src-ui/app/main_page/sidebar_section/SidebarSection.jsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 
 import styles from "./SidebarSection.module.scss";
-import { useStore_IsMainPageCompactMode } from "@store";
+import { useStore_IsMainPageCompactMode, useStore_IsOpenedLanguageSelector } from "@store";
 
 import { Logo } from "./logo/Logo";
 import { MainFunctionSwitch } from "./main_function_switch/MainFunctionSwitch";
@@ -14,10 +14,15 @@ export const SidebarSection = () => {
         [styles.is_compact_mode]: currentIsMainPageCompactMode
     });
 
+    const { currentIsOpenedLanguageSelector } = useStore_IsOpenedLanguageSelector();
+    const scroll_container_class_names = clsx(styles.scroll_container, {
+        [styles.is_opened]: (currentIsOpenedLanguageSelector.your_language === true || currentIsOpenedLanguageSelector.target_language === true)
+    });
+
     return (
         <div className={container_class_name}>
             <Logo />
-            <div className={styles.scroll_container}>
+            <div className={scroll_container_class_names}>
                 <MainFunctionSwitch />
                 {!currentIsMainPageCompactMode && <LanguageSettings />}
             </div>
