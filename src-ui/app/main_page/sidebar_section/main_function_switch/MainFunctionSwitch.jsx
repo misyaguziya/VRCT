c0ba530d328e6bb3749cd661f4bfeae69d889686
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-09-23 10:21:49 +0900

    [Update] Main Page: Connect to backend. IsMainPageCompactMode.

--- src-ui/app/main_page/sidebar_section/main_function_switch/MainFunctionSwitch.jsx
+++ src-ui/app/main_page/sidebar_section/main_function_switch/MainFunctionSwitch.jsx
@@ -5,7 +5,7 @@ import TranslationSvg from "@images/translation.svg?react";
 import MicSvg from "@images/mic.svg?react";
 import HeadphonesSvg from "@images/headphones.svg?react";
 import ForegroundSvg from "@images/foreground.svg?react";
-import { useStore_IsMainPageCompactMode } from "@store";
+import { useIsMainPageCompactMode } from "@logics_main/useIsMainPageCompactMode";
 
 import { useMainFunction } from "@logics_main/useMainFunction";
 
@@ -74,7 +74,7 @@ export const SwitchContainer = ({ switchLabel, switch_id, children, currentState
     const [is_hovered, setIsHovered] = useState(false);
     const [is_mouse_down, setIsMouseDown] = useState(false);
 
-    const { currentIsMainPageCompactMode } = useStore_IsMainPageCompactMode();
+    const { currentIsMainPageCompactMode } = useIsMainPageCompactMode();
 
     const getClassNames = (baseClass) => clsx(baseClass, {
         [styles.is_compact_mode]: currentIsMainPageCompactMode.data,
