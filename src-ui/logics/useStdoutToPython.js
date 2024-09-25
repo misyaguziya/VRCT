d8f6fbef3065c827bf3ea0a8cd551271e7068cd3
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-09-25 23:42:18 +0900

    [bugfix] useStdoutToPython: fix the send data that can't send 0, int, data.

--- src-ui/logics/useStdoutToPython.js
+++ src-ui/logics/useStdoutToPython.js
@@ -2,9 +2,9 @@ import { store } from "@store";
 import { encode } from "js-base64";
 
 export const useStdoutToPython = () => {
-    const asyncStdoutToPython = async (path, value) => {
+    const asyncStdoutToPython = async (path, value = undefined) => {
         let send_object = { endpoint: path };
-        if (value) send_object.data = encode(JSON.stringify(value));
+        if (value !== undefined) send_object.data = encode(JSON.stringify(value));
 
         // send to python
         const backend_subprocess = store.backend_subprocess;
