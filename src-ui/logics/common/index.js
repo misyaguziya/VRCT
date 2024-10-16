2f6a9d4f2b86b29dfe0315e53dbf90f5887c6762
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-10-16 23:50:55 +0900

    [Update] Config Page: Auto Export Message Logs. Add open folder button.

--- src-ui/logics/common/index.js
+++ src-ui/logics/common/index.js
@@ -1,4 +1,5 @@
 export { useWindow } from "./useWindow";
 export { useIsOpenedConfigPage } from "./useIsOpenedConfigPage";
+export { useOpenFolder } from "./useOpenFolder";
 export { useMessage } from "./useMessage";
 export { useVolume } from "./useVolume";
