3c769021240f782c43155466e767559c3d21677a
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-10-30 16:01:37 +0900

    [Update] Quick Settings: Add Overlay settings.

--- src-ui/app/main_page/main_section/top_bar/right_side_components/RightSideComponents.jsx
+++ src-ui/app/main_page/main_section/top_bar/right_side_components/RightSideComponents.jsx
@@ -1,12 +1,15 @@
 import styles from "./RightSideComponents.module.scss";
-
 import HelpSvg from "@images/help.svg?react";
+
+import { useStore_OpenedQuickSetting } from "@store";
+import { useIsEnabledOverlaySmallLog } from "@logics_configs";
+import { OpenQuickSettingButton } from "./_buttons/OpenQuickSettingButton";
 
 export const RightSideComponents = () => {
     return (
         <div className={styles.container}>
             <p>VRC mic mute sync</p>
-            <p>Overlay(VR)</p>
+            <OpenOverlayQuickSetting />
             <a
             className={styles.help_and_info_button}
             href="https://mzsoftware.notion.site/VRCT-Documents-be79b7a165f64442ad8f326d86c22246"
@@ -16,5 +19,22 @@ export const RightSideComponents = () => {
                 <HelpSvg className={styles.help_svg} />
             </a>
         </div>
+    );
+};
+
+const OpenOverlayQuickSetting = () => {
+    const { currentOpenedQuickSetting, updateOpenedQuickSetting } = useStore_OpenedQuickSetting();
+    const { currentIsEnabledOverlaySmallLog } = useIsEnabledOverlaySmallLog();
+
+    const onClickFunction = () => {
+        updateOpenedQuickSetting("overlay");
+    };
+
+    return (
+        <OpenQuickSettingButton
+            label="Overlay(VR)"
+            variable={currentIsEnabledOverlaySmallLog.data}
+            onClickFunction={onClickFunction}
+        />
     );
 };
