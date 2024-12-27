b3a55eb88b09dde60329d979501398c175123e26
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-12-28 05:24:52 +0900

    [Update] Main Page: Translator: Add warning ui to translate selector when the same languages are selected.

--- src-ui/app/main_page/sidebar_section/language_settings/translator_selector_open_button/TranslatorSelectorOpenButton.jsx
+++ src-ui/app/main_page/sidebar_section/language_settings/translator_selector_open_button/TranslatorSelectorOpenButton.jsx
@@ -4,10 +4,13 @@ import styles from "./TranslatorSelectorOpenButton.module.scss";
 import { TranslatorSelector } from "./translator_selector/TranslatorSelector";
 import { useStore_IsOpenedTranslatorSelector } from "@store";
 import { useLanguageSettings } from "@logics_main";
+import WarningSvg from "@images/warning.svg?react";
 
 export const TranslatorSelectorOpenButton = () => {
     const { t } = useTranslation();
     const {
+        currentSelectedYourLanguages,
+        currentSelectedTargetLanguages,
         currentSelectedPresetTabNumber,
         currentTranslationEngines,
         currentSelectedTranslationEngines,
@@ -20,6 +23,35 @@ export const TranslatorSelectorOpenButton = () => {
     const translation_engines = updateLabelsById(currentTranslationEngines.data, new_labels);
 
     const selected_engine_id = currentSelectedTranslationEngines.data[currentSelectedPresetTabNumber.data];
+
+    const checkIsSelectedSameLanguage = () => {
+        const your_language_data = currentSelectedYourLanguages.data[currentSelectedPresetTabNumber.data];
+        const target_language_data = currentSelectedTargetLanguages.data[currentSelectedPresetTabNumber.data];
+
+        const yourLanguage = your_language_data["1"];
+        const yourLanguageName = yourLanguage.language;
+        const yourCountry = yourLanguage.country;
+
+        let is_selected_same_language = false;
+
+        for (const key in target_language_data) {
+            const targetLanguage = target_language_data[key];
+
+            if (targetLanguage.enable) {
+                const targetLanguageName = targetLanguage.language;
+                const targetCountry = targetLanguage.country;
+
+                if (yourLanguageName === targetLanguageName && yourCountry === targetCountry) {
+                    is_selected_same_language = true;
+                    break;
+                }
+            }
+        }
+
+        return is_selected_same_language;
+    };
+
+    const is_selected_same_language = checkIsSelectedSameLanguage();
 
     const getSelectedLabel = () => {
 
@@ -44,11 +76,16 @@ export const TranslatorSelectorOpenButton = () => {
             <div className={styles.translator_selector_button} onClick={openTranslatorSelector}>
                 <p className={styles.label}>{t("main_page.translator")}: </p>
                 <p className={styles.label}>{selected_label}</p>
+                {is_selected_same_language
+                    ? <WarningSvg className={styles.warning_svg}/>
+                    : null
+                }
             </div>
             {currentIsOpenedTranslatorSelector.data &&
                 <TranslatorSelector
                     selected_id={selected_engine_id}
                     translation_engines={translation_engines}
+                    is_selected_same_language={is_selected_same_language}
                 />
             }
         </div>
