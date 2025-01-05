faec2f2e55b00a1303b429082a900e5bd94ea002
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2025-01-05 14:23:10 +0900

    [Update/TMP] The shortcut will trigger whenever.(Remove the checking whether the window is VRChat or not)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,9 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// use tauri::command;
 use tauri::Manager;
 use window_shadows::set_shadow;
-use windows::Win32::UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowTextW};
-use std::collections::HashSet;
-
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
@@ -12,16 +13,18 @@ fn main() {
             { main_window.open_devtools(); }
 
             #[cfg(any(windows, target_os = "macos"))]
-            set_shadow(main_window, true).unwrap()
+            set_shadow(main_window, true).unwrap();
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![get_font_list, get_active_window_title])
+        .invoke_handler(tauri::generate_handler![get_font_list])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
+
 use font_kit::{source::SystemSource};
+use std::collections::HashSet;
 
 #[tauri::command]
 async fn get_font_list() -> Vec<String> {
@@ -37,28 +40,4 @@ async fn get_font_list() -> Vec<String> {
     }
 
     font_families.into_iter().collect()
-}
-
-use windows::Win32::Foundation::HWND;
-#[tauri::command]
-fn get_active_window_title() -> String {
-    unsafe {
-        // Get the handle of the foreground (active) window
-        let hwnd = GetForegroundWindow();
-        if hwnd == HWND(0) {  // `HWND(0)` は null ハンドルを表します
-            return "No active window".to_string();
-        }
-
-        // Create a buffer to hold the window title
-        let mut buffer: [u16; 512] = [0; 512];
-
-        // Get the window title
-        let length = GetWindowTextW(hwnd, &mut buffer);
-
-        if length > 0 {
-            // Convert the result to a Rust string
-            return String::from_utf16_lossy(&buffer[..length as usize]);
-        }
-    }
-    "Failed to retrieve window title".to_string()
 }
--- src-ui/app/_app_controllers/GlobalHotKeyController.jsx
+++ src-ui/app/_app_controllers/GlobalHotKeyController.jsx
@@ -1,31 +1,23 @@
     import { appWindow } from "@tauri-apps/api/window";
     import { register, unregisterAll, isRegistered } from "@tauri-apps/api/globalShortcut";
-    import { invoke } from "@tauri-apps/api/tauri";
     import { useEffect, useRef } from "react";
     import { store } from "@store";
 
     export const GlobalHotKeyController = () => {
         const is_initialized = useRef(false);
-
         useEffect(() => {
             if (is_initialized.current) return;
 
             const registerShortcuts = async () => {
                 const shortcut = "Alt+Y";
                 const is_already_registered = await isRegistered(shortcut);
-
                 if (is_already_registered) return;
-                await register(shortcut, async () => {
-                    const activeWindowTitle = await invoke("get_active_window_title");
 
-                    if (activeWindowTitle.includes("VRChat")) {
-                        console.log("Shortcut triggered, setFocus");
-                        appWindow.unminimize();
-                        await appWindow.setFocus();
-                        store.text_area_ref.current?.focus();
-                    } else {
-                        console.log("Active window is not VRChat.");
-                    }
+                await register(shortcut, async () => {
+                    console.log("Shortcut triggered, setFocus");
+                    appWindow.unminimize();
+                    await appWindow.setFocus();
+                    store.text_area_ref.current?.focus();
                 });
             };
 
