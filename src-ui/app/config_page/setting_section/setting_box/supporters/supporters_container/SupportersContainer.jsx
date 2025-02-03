34f6bb215a021b229b0cc5a12e9672eee046589c
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2025-02-03 14:08:07 +0900

    [Update] Config Page: supporters: Update-able supporters title calc period.

--- src-ui/app/config_page/setting_section/setting_box/supporters/supporters_container/SupportersContainer.jsx
+++ src-ui/app/config_page/setting_section/setting_box/supporters/supporters_container/SupportersContainer.jsx
@@ -2,6 +2,7 @@ import styles from "./SupportersContainer.module.scss";
 import { SupportersWrapper } from "./supporters_wrapper/SupportersWrapper";
 import { useSupporters } from "@logics_configs";
 import { supporters_images_url } from "@ui_configs";
+import vrct_supporters_title from "@images/supporters/vrct_supporters_title.png";
 
 export const SupportersContainer = () => {
     const { currentSupportersData } = useSupporters();
@@ -14,7 +15,10 @@ export const SupportersContainer = () => {
 
     return (
         <div className={styles.supporters_container}>
-            <img className={styles.vrct_supporters_title} src={`${supporters_images_url}/vrct_supporters_title.png`} />
+            <div className={styles.vrct_supporters_title_wrapper}>
+                <img className={styles.vrct_supporters_title} src={vrct_supporters_title}/>
+                <img className={styles.calc_period} src={`${supporters_images_url}/calc_period_label.png`}/>
+            </div>
             <SupportersWrapper />
             <p className={styles.vrct_supporters_desc_end}>{`みなさんのおかげで、みしゃ社長は布団で寝ることを許され(in開発室) しいなは喜び庭駆け回っています！！！ふわもちもぐもぐです！ありがとうございます。これからもまだまだ進化するVRCTをどうかよろしくお願いします！\nThanks to everyone, Misha has been granted the privilege of sleeping in a proper bed (in the development room), and Shiina is so happy, running around the yard! Fuwa-mochi-mogu-mogu! Thank you so much! We hope you'll continue to support the ever-evolving VRCT!`}</p>
         </div>
