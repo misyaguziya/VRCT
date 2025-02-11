cd4647fa6208751ea45ebf3814d2f061e84f312f
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2025-02-11 15:21:57 +0900

    [bugfix] GlobalHotKeyController: Fix the bug that even if VRCT is not available, (that when ai models has not been detected), registered the hotkeys works and could turn on main functions.

--- src-ui/app/_app_controllers/GlobalHotKeyController.jsx
+++ src-ui/app/_app_controllers/GlobalHotKeyController.jsx
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import { useHotkeys } from "@logics_configs";
-import { useIsBackendReady, useIsSoftwareUpdating } from "@logics_common";
+import { useIsBackendReady, useIsSoftwareUpdating, useIsVrctAvailable } from "@logics_common";
 
 export const GlobalHotKeyController = () => {
     const { currentIsBackendReady } = useIsBackendReady();
     const { currentIsSoftwareUpdating } = useIsSoftwareUpdating();
     const { registerShortcuts, unregisterAll } = useHotkeys();
 
+    const { currentIsVrctAvailable } = useIsVrctAvailable();
+
     useEffect(() => {
         const is_backend_ready = currentIsBackendReady.data;
         const is_software_updating = currentIsSoftwareUpdating.data;
+        const is_vrct_available = currentIsVrctAvailable.data;
 
-        if (is_backend_ready && !is_software_updating) {
+        if (is_vrct_available && is_backend_ready && !is_software_updating) {
             registerShortcuts();
-        } else if (is_software_updating) {
+        } else {
             unregisterAll();
         }
-    }, [currentIsBackendReady.data, currentIsSoftwareUpdating.data]);
+    }, [currentIsBackendReady.data, currentIsSoftwareUpdating.data, currentIsVrctAvailable.data]);
 
     return null;
 };
