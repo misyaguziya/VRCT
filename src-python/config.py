62571f55a1d76871c3a28f739045e84d256bca7a
misyaguziya <53165965+misyaguziya@users.noreply.github.com>
2025-02-19 13:38:55 +0900

    👍️[Update] Version 3.0.2 -> 3.0.3

--- src-python/config.py
+++ src-python/config.py
@@ -944,7 +944,7 @@ class Config:
 
     def init_config(self):
         # Read Only
-        self._VERSION = "3.0.2"
+        self._VERSION = "3.0.3"
         if getattr(sys, 'frozen', False):
             self._PATH_LOCAL = os_path.dirname(sys.executable)
         else:
