73c6afa5e1e953100e57352f3f5931f76b608d2b
lizhirui <exiis@126.com>
2025-04-22 01:22:52 +0800

    add rtx5000 series support for ai model

--- install.bat
+++ install.bat
@@ -7,4 +7,5 @@ pip install -r requirements.txt
 
 call .venv_cuda/Scripts/activate
 python.exe -m pip install --upgrade pip
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
 pip install -r requirements_cuda.txt
