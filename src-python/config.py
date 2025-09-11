21e1a7f765add5ddc0772567ae75d85c82015f79
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2025-09-11 17:00:15 +0900

    [Update] Version 3.2.1 -> 3.2.2

--- src-python/config.py
+++ src-python/config.py
@@ -1020,7 +1020,7 @@ class Config:
 
     def init_config(self):
         # Read Only
-        self._VERSION = "3.2.1"
+        self._VERSION = "3.2.2"
         if getattr(sys, 'frozen', False):
             self._PATH_LOCAL = os_path.dirname(sys.executable)
         else:
