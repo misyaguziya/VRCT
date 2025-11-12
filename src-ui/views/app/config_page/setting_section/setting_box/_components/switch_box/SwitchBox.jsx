ddcd19d5ebc7ca14a960adbca234ac9f85d2bdfa
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2025-11-12 15:27:10 +0900

    [Refactor] Separate SwitchBox into _atom and wrapper, add secondary label support.

--- src-ui/views/app/config_page/setting_section/setting_box/_components/switch_box/SwitchBox.jsx
+++ src-ui/views/app/config_page/setting_section/setting_box/_components/switch_box/SwitchBox.jsx
@@ -1,44 +1,13 @@
 import clsx from "clsx";
 import { useState } from "react";
 import styles from "./SwitchBox.module.scss";
+import { _SwitchBox } from "../_atoms/_switch_box/_SwitchBox";
 
 export const SwitchBox = (props) => {
-    const [is_hovered, setIsHovered] = useState(false);
-    const [is_mouse_down, setIsMouseDown] = useState(false);
-
-    const is_pending = (props.variable.state === "pending");
-
-    const getClassNames = (base_class) => clsx(base_class, {
-        [styles.is_active]: (props.variable.data === true),
-        [styles.is_pending]: is_pending,
-        [styles.is_hovered]: is_hovered,
-        [styles.is_mouse_down]: is_mouse_down,
-    });
-
-    const onMouseEnter = () => setIsHovered(true);
-    const onMouseLeave = () => setIsHovered(false);
-    const onMouseDown = () => setIsMouseDown(true);
-    const onMouseUp = () => setIsMouseDown(false);
-
-    const toggleFunction = () => {
-        props.toggleFunction();
-    };
-
-
     return (
-        <div className={styles.switchbox_container}>
-            <div className={getClassNames(styles.switchbox_wrapper)}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-                onMouseDown={onMouseDown}
-                onMouseUp={onMouseUp}
-                onClick={toggleFunction}
-            >
-                <div className={getClassNames(styles.toggle_control)}>
-                    <span className={getClassNames(styles.control)}></span>
-                    {is_pending && <span className={styles.loader}></span>}
-                </div>
-            </div>
+        <div className={styles.container}>
+            {props.secondary_label && <p className={styles.secondary_label}>{props.secondary_label}</p>}
+            <_SwitchBox {...props} />
         </div>
     );
 };
