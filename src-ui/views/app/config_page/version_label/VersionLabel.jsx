be8782f63310acb7b200f3a4ebe638c024755bba
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2026-02-27 07:09:47 +0900

    [Update] UI: Enhance ConfigPage SidebarSection interactivity; add new SVG assets for improved visual elements.

--- src-ui/views/app/config_page/version_label/VersionLabel.jsx
+++ src-ui/views/app/config_page/version_label/VersionLabel.jsx
@@ -7,41 +7,45 @@ import { useSoftwareVersion, useComputeMode } from "@logics_common";
 import CopySvg from "@images/copy.svg?react";
 import CheckMarkSvg from "@images/check_mark.svg?react";
 
-export const VersionLabel = () => {
+export const VersionLabel = ({ isCompact = false }) => {
     const [is_copied, setIsCopied] = useState(false);
 
     const { t } = useI18n();
     const { currentSoftwareVersion } = useSoftwareVersion();
     const { currentComputeMode } = useComputeMode();
 
-    const version_label = currentComputeMode.data === "cpu"
-        ? t("config_page.common.version", { version: currentSoftwareVersion.data })
-        : currentComputeMode.data === "cuda"
-        ? t("config_page.common.version", { version: currentSoftwareVersion.data }) + " CUDA"
-        : t("config_page.common.version", { version: currentSoftwareVersion.data });
+    const is_cuda = currentComputeMode.data === "cuda";
+    const software_version_number = currentSoftwareVersion.data;
 
-        const is_cpu = currentComputeMode.data === "cpu";
+    const version_label = (
+        <div className={clsx(styles.version_text_container, {[styles.is_compact]: isCompact})}>
+            <p className={styles.version_label}>{`v${software_version_number}`}</p>
+            {is_cuda && <p className={styles.cuda_label}> CUDA</p>}
+        </div>
+    );
 
-        const copyToClipboard = async () => {
-            if (is_copied) return;
-            const copy_text = is_cpu ? `${currentSoftwareVersion.data}` : `${currentSoftwareVersion.data} CUDA`;
-            await navigator.clipboard.writeText(copy_text);
-            setIsCopied(true);
+    const is_cpu = currentComputeMode.data === "cpu";
 
-            setTimeout(() => {
-                setIsCopied(false);
-            }, 1000);
-        };
+    const copyToClipboard = async () => {
+        if (is_copied || isCompact) return;
+        const copy_text = is_cpu ? `${software_version_number}` : `${software_version_number} CUDA`;
+        await navigator.clipboard.writeText(copy_text);
+        setIsCopied(true);
 
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 1000);
+    };
 
     return (
-        <div className={styles.container}>
-            <div className={clsx(styles.wrapper, {[styles.is_copied]: is_copied})} onClick={copyToClipboard}>
-                <p className={styles.version_label}>{version_label}</p>
-                {is_copied
-                    ? <CheckMarkSvg className={styles.check_mark_svg}/>
-                    : <CopySvg className={styles.copy_svg}/>
-                }
+        <div className={clsx(styles.container, { [styles.is_compact]: isCompact })}>
+            <div className={clsx(styles.wrapper, {[styles.is_copied]: is_copied, [styles.is_compact]: isCompact})} onClick={copyToClipboard}>
+                {version_label}
+                {!isCompact && (
+                    is_copied
+                        ? <CheckMarkSvg className={styles.check_mark_svg}/>
+                        : <CopySvg className={styles.copy_svg}/>
+                )}
             </div>
         </div>
     );
